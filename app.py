367c823bd6e1f6dd5d2bf6ecee6e24c9c74ebd89
JANUS <ritviktyagi1@gmail.com>
2020-10-11 08:44:45 +0530

    INITIAL

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask ,render_template,request
+import pickle
+import numpy as np
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def initial():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    prediction=model.predict(final)
+    if prediction==1:
+        return render_template('index.html',pred='You have a high chance of contracting cardiovascular disease. You should start exercising')
+    else:
+        return render_template('index.html',pred='You have a low chance of contracting cardiovascular disease. But dont forget to exercise')
+if __name__=='main':
+    app.run()
---
+++ model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+warnings.filterwarnings("ignore")
+df = pd.read_csv("../data/heart.csv")
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+y = df.target.values
+x_data = df.drop(['target'], axis = 1)
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values# Normalize
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
+x_train = x_train.T
+y_train = y_train.T
+x_test = x_test.T
+y_test = y_test.T
+rf = RandomForestClassifier(n_estimators = 1000, random_state = 1)
+rf.fit(x_train.T, y_train.T)
+pickle.dump(rf,open('model.pkl','wb'))
+model=pickle.load(open('model.pkl','rb'))
